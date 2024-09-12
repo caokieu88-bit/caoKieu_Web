6a4d4292d9da22b648349a9c1ada58aa4c688d07
CaoKieu <caokieu88@gmail.com>
2024-09-12 17:17:05 +0700

    lan6-09.12

---
+++ main.js
@@ -0,0 +1,25 @@
+const videoList = document.getElementById("videoList");
+const videoPlayer = document.getElementById("myVideo");
+const searchInput = document.getElementById("searchInput");
+
+videoList.addEventListener("click", (event) => {
+  event.preventDefault();
+  const videoSrc = event.target.dataset.video;
+  videoPlayer.src = videoSrc;
+  videoPlayer.style.display = "block";
+  videoPlayer.play();
+});
+
+searchInput.addEventListener("input", (event) => {
+  const searchTerm = event.target.value.toLowerCase();
+  const videos = videoList.querySelectorAll("li");
+
+  videos.forEach((video) => {
+    const title = video.textContent.toLowerCase();
+    if (title.includes(searchTerm)) {
+      video.style.display = "block";
+    } else {
+      video.style.display = "none";
+    }
+  });
+});
